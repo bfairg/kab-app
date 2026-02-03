56c977fa165d2c11dc73bbd7f197e7fa24b71793
bfairg <barryfairgrieve@hotmail.com>
2026-02-03 22:55:54 +0000

    Update page.tsx

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { Suspense } from "react";
 import SignupClient from "./SignupClient";
 import { redirectIfCustomerClaimed } from "@/lib/guards/redirectIfCustomerClaimed";
