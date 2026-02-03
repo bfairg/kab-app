7e4eb4956ddbf5c8f63278055cfee945d8287918
bfairg <barryfairgrieve@hotmail.com>
2026-02-03 22:56:41 +0000

    Update page.tsx

--- app/signup/completion/page.tsx
+++ app/signup/completion/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { Suspense } from "react";
 import CompletionClient from "./CompletionClient";
 import { redirectIfCustomerClaimed } from "@/lib/guards/redirectIfCustomerClaimed";
